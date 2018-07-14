8a838f93307eec2c7c7c8b637ace933a0e5b6d24
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-14 10:44:17 +0100

    add actions

---
+++ redux/actions/categoriesActions.js
@@ -0,0 +1,43 @@
+
+import * as Types from './types'
+import * as api from '../../helpers/api'
+
+export const fetchCategories = () => {
+  return async dispatch => {
+    dispatch({ type: Types.FETCH_CATEGORIES })
+
+    try {
+      const { data: categories } = await api.getCategories()
+
+      dispatch({
+        type: Types.FETCH_CATEGORIES_SUCCESS,
+        categories
+      })
+    } catch (err) {
+      dispatch({
+        type: Types.FETCH_CATEGORIES_ERROR,
+        error: err
+      })
+    }
+  }
+}
+
+export const fetchArticles = (categoryId) => {
+  return async dispatch => {
+    // dispatch({type: Types.FETCH_ARTICLES})
+
+    try {
+      const { data: articles } = await api.getArticles(categoryId)
+
+      dispatch({
+        type: Types.FETCH_ARTICLES_SUCCESS,
+        articles
+      })
+    } catch (err) {
+      dispatch({
+        type: Types.FETCH_ARTICLES_ERROR,
+        error: err
+      })
+    }
+  }
+}
