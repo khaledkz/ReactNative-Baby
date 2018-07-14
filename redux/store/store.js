fe23d48b0c59545c00df261bec3c48804990f5c5
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-14 10:45:01 +0100

    add store

---
+++ redux/store/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+ import rootReducer from './rootReducer'
+
+// Note: this API requires redux@>=3.1.0
+const store = createStore(
+  rootReducer
+)
+
+export default store
